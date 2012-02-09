f048111e920be122bd782b7cba9b9c667463fa3f
Maciej Litwiniuk <maciej@galdomedia.pl>
2012-02-09 21:47:27 +0100

    correct indentation

--- generators/timeline_fu/templates/model.rb
+++ generators/timeline_fu/templates/model.rb
@@ -3,5 +3,5 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :actor,              :polymorphic => true
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
-  belongs_to :extra_scope,              :polymorphic => true
+  belongs_to :extra_scope,        :polymorphic => true
 end
